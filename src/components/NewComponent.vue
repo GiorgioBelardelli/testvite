1ba4950775e4535e6218e1851f855b830691b158
GiorgioBelardelli <asterapocalypse@gmail.com>
2023-12-20 15:23:08 +0100

    feat: NewComponent h1

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "NewComponent"
+}
+</script>
+
+<template>
+    <h1 class="read-the-docs">La mia prima App con Vite!</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
